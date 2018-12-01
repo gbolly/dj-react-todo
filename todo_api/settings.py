be1f87c2821ddbb9a67674cb2fb5f9e7298f0fde
gbolly <okerayigbolahan@gmail.com>
2018-12-01 23:29:43 +0100

    Setup DRF for api

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -37,13 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'todos'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -119,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/'
+)
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='first todo')
+        Todo.objects.create(description='a description here')
+        Todo.objects.create(status=False)
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEquals(expected_object_name, 'first todo')
+
+    def test_description_content(self):
+        todo = Todo.objects.get(id=2)
+        expected_object_name = f'{todo.description}'
+        self.assertEquals(expected_object_name, 'a description here')
+
+    def test_status_content(self):
+        todo = Todo.objects.get(id=3)
+        expected_object_name = f'{todo.status}'
+        self.assertEquals(expected_object_name, 'False')
